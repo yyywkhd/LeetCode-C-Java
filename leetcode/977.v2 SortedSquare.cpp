300160837a9d1eb1706ff77b02702c24457c9713
yyywkhd <peiyan.chen1999@gmail.com>
2023-02-03 11:06:25 +1030

    update #977

---
+++ leetcode/977.v2 SortedSquare.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int k = A.size() - 1;
+        vector<int> result(A.size(), 0);
+        for (int i = 0, j = A.size() - 1; i <= j;) { 
+            if (A[i] * A[i] < A[j] * A[j])  {
+                result[k--] = A[j] * A[j];
+                j--;
+            }
+            else {
+                result[k--] = A[i] * A[i];
+                i++;
+            }
+        }
+        return result;
+    }
+};
