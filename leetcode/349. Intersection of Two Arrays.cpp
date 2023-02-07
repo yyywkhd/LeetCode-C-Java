73226ae30da845c9e7480e4655753c5b13bc2499
yyywkhd <peiyan.chen1999@gmail.com>
2023-02-07 11:28:32 +1030

    Update 349. Intersection of Two Arrays.cpp

--- leetcode/349. Intersection of Two Arrays.cpp
+++ leetcode/349. Intersection of Two Arrays.cpp
@@ -13,19 +13,4 @@ public:
         }
         return vector<int>(result.begin(),result.end());
     }
-};class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> result;
-        int hash[1005] = {0};
-        for (int num : nums1){
-            hash[num] = 1;
-        }
-        for (int num : nums2){
-            if(hash[num] == 1){
-                result.insert(num);
-            }
-        }
-        return vector<int>(result.begin(),result.end());
-    }
 };
